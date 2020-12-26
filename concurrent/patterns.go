3ef367d73fc3a4ff50e62f355e0d4a56c934549d
Rahul Goel <rahul.usit12@gmail.com>
2020-12-26 10:52:09 +0530

    adding two go concurrency patterns

--- concurrent/patterns.go
+++ concurrent/patterns.go
@@ -7,6 +7,8 @@ import (
 )
 
 func pattern1(){
+	// normal channels (not buffered) will block until there is a msg in channel,
+	//producer and consumer statements will block
 	c := make(chan string)
 	go boring1("Boring!", c)
 	for i := 0; i < 5; i++{
@@ -22,7 +24,41 @@ func boring1(msg string, c chan string) {
 	}
 }
 
+func pattern2(){
+	c := boring2("boring!")
+	for i :=0; i < 5; i++{
+		fmt.Printf("You say: %q\n", <-c)
+	}
+	fmt.Println("You're boring; I'm leaving")
+}
+
+func boring2(msg string) <-chan string{
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++{
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return c
+}
+
+
+func pattern2Dash(){
+	// this is channels as a handle on a service
+	// both are blocking each other, not a good design if not required explicitly
+	joe := boring2("Joe!")
+	ann := boring2("Ann!")
+
+	for i :=0; i < 5; i++{
+		fmt.Printf("You say: %q\n", <-joe)
+		fmt.Printf("You say: %q\n", <-ann)
+	}
+	fmt.Println("You're boring; I'm leaving")
+}
 
 func TestGoConcurrencyPatterns(){
-	pattern1()
+	//pattern1()
+	//pattern2()
+	pattern2Dash()
 }
